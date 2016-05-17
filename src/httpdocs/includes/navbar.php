43e483ec8c04551be4a8ca521935d35144ed70af
adam j martin <adam.ajm@gmail.com>
2016-05-16 21:24:04 -0400

    home page update text changes

--- src/httpdocs/includes/navbar.php
+++ src/httpdocs/includes/navbar.php
@@ -1,6 +1,6 @@
 <div class="container wrapper">
   <div class="alert alert-danger" align="center">            
-    The budget information in the visualizations throughout this website reflect the <strong>PROPOSED</strong> revenue, expenditure and capital budgets for Fiscal Year 2017.    
+    The budget information in the visualizations throughout this website reflect the <strong>PROPOSED</strong> revenue, expenditure and capital budgets for Fiscal Year 2017, as of May 17, 2016. City Council will vote on the proposed budget by June 30.   
     <br> 
     We welcome you to <a class="link" href="https://docs.google.com/forms/d/1fZiTbLURyLWOoval69mD5_nHSyAIJTiu9DbcKXKwDbQ/viewform">send us your feedback</a>! 
   </div>
